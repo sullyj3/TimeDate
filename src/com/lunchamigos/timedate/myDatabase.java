ef24487153acac9cdad17d967fb3588ed2fb691c
James Sully <sullyj3@gmail.com>
2014-03-26 17:51:29 +1100

    forgot a semicolon

--- src/com/lunchamigos/timedate/myDatabase.java
+++ src/com/lunchamigos/timedate/myDatabase.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import android.content.ContentValues;
 import android.content.Context;
-import android.database.Cursor
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
