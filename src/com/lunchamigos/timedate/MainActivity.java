0ecba8b8fde33b64ae1bb440134ccbac87263d4f
James Sully <sullyj3@gmail.com>
2014-03-26 18:01:54 +1100

    nother semicolon

--- src/com/lunchamigos/timedate/MainActivity.java
+++ src/com/lunchamigos/timedate/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-import android.string.format.Time
+import android.string.format.Time;
 
 public class MainActivity extends ActionBarActivity {
 
