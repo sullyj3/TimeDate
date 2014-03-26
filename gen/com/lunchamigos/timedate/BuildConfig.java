47843bdb47178ec4daa7d3b78c8f2973420b6b8c
James Sully <sullyj3@gmail.com>
2014-03-26 18:37:53 +1100

    clean

--- gen/com/lunchamigos/timedate/BuildConfig.java
+++ gen/com/lunchamigos/timedate/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.luchamigos.timedate;
+package com.lunchamigos.timedate;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
