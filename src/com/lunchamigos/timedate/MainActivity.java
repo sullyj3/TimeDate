ce8d6908b710581c95640e997181eb4e4fb07d25
James Sully <sullyj3@gmail.com>
2014-03-26 18:27:29 +1100

    forgot an 'n'

--- src/com/lunchamigos/timedate/MainActivity.java
+++ src/com/lunchamigos/timedate/MainActivity.java
@@ -1,6 +1,6 @@
 package com.lunchamigos.timedate;
 
-import com.luchamigos.timedate.R;
+import com.lunchamigos.timedate.R;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
